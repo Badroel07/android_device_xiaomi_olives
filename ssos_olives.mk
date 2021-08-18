dad93e25c627f29ab13c60ab999a37c91709e4af
Chuck439 <firmankediri123@gmail.com>
2021-08-18 21:54:27 +0700

    Revert ShapeShift changes
    This reverts commit 686808a2046e34c98ab4651980bb61fdc7612b6d.

--- ssos_olives.mk
+++ ssos_olives.mk
@@ -12,8 +12,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit some common ssos stuff.
 $(call inherit-product, vendor/ssos/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 720
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.ssos.cpu=SDM439
 
 # Inherit from olives device
 $(call inherit-product, device/xiaomi/olives/device.mk)
